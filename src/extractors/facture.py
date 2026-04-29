0ebec75572fc88640e741bbabe5ee58ac82d8c0e
samirarrous <samirarous2004@gmail.com>
2026-04-29 18:41:11 +0200

    fixed company name extraction and added some other the company name is often before the jurridical form, but not always,  so i noticed that it's either the document type or the company name that is on the first line,  so i decided to check if the first line contains the document type,  if it does, then we will search for the company name in the rest of the text,  otherwise we will consider that the first line as the company name, added extraction of exercice clos date and TVA number

--- src/extractors/facture.py
+++ src/extractors/facture.py
@@ -5,11 +5,8 @@ import os
 # Ajouter le dossier parent au chemin
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-import text_extractor
-import pdf_type
 
 def extract_facture_number(text):
-    # On autorise des espaces partout, même autour du slash
     pattern = r"n\s*°?\s*:?\s*((?:[A-Z]+\s*[-/]\s*)?\d{4}\s*[-/]\s*\d{3,4})"# example: N° ABC-2024-001
                              # optional ° or : after N   
                              # \s* in case OCR added spaces (AB   -   2026 - 001)
@@ -17,13 +14,26 @@ def extract_facture_number(text):
                              # [A-Z]* optional letters
     match = re.search(pattern, text, re.IGNORECASE)
     if match:
-        return match.group(1)  # Return the captured facture number
+        return match.group(1).upper  
     return "unknown"
     
 def extract_limit_date(text):
     pattern = r"(échéance|avant le|limite) \s*:\s*(\d{2}\s*[/-]\s*\d{2}\s*[/-]\s*\d{4})"
     match = re.search(pattern, text, re.IGNORECASE)
     if match:
-        return match.group(2)  # Return the captured date
+        return match.group(2)  
     return "unknown"
-print("text :", text_extractor.extract_text("../../sample_pdfs/03_facture_plomberie_scan.pdf"))
+
+def extract_SIRET(text):
+    pattern = r"SIRET\s*[:]?\s*(\d{3}\s*\d{3}\s*\d{3}\s*\d{5})"
+    match = re.search(pattern, text, re.IGNORECASE)
+    if match:
+        return match.group(1)
+    return "unknown"
+
+def extract_tva_number(text):
+    pattern = r"TVA\s*([A-ZÀ-ÿ\s'-:]+?)\s*([A-Z]{2}\s*\d{2}\s*\d{3}\s*\d{3})"
+    match = re.search(pattern, text, re.IGNORECASE)
+    if match:
+        return match.group(2)
+    return "unknown"
