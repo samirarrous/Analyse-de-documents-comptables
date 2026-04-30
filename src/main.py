f19dbbdc69880c677ffd0589c4089675eb4c8699
samirarrous <samirarous2004@gmail.com>
2026-04-30 16:14:29 +0200

    fixing CI/CD , exportation of files in json choosed to extract all fields in each document type for a better structure added a router for a better structure

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from pipeline.router import route_document
-from pipeline.exporter import export_to_json
-
-file = "../sample_pdfs/06_bilan_delices_scan.pdf"
-fields = route_document(file)
-
-path = export_to_json(fields)
-
-print(export_to_json(fields))
